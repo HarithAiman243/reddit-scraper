7981b073e7b7014c090c3b349fd07d3b2d663768
Harith Aiman <mharithaiman243@gmail.com>
2025-08-29 09:41:59 +0800

    Add for loop between '10 pages', Reddit has infinite scrolling. Used limit=25 for limiting post for each page.

--- scraper.py
+++ scraper.py
@@ -4,3 +4,14 @@ import json
 
 headers = {'User-agent': 'Mozilla/5.0'}
 url = "https://www.reddit.com/r/malaysia/top.json?t=week"  # Filter by Top Post, Week
+
+pages = []  # list of pages, each page is a list of posts
+after = None
+
+for i in range(10):  # simulate 10 pages
+    params = {
+        'after': after,
+        'limit': 25   # fetch 25 posts per request
+    }
+    res = requests.get(url, headers=headers, params=params)
+    data = res.json()
